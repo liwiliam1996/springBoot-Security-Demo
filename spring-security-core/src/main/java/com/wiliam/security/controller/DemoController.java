3be1fedbbbb50ddc1769a052543058aa37264e02
lieq <lieq@njzdgt.com>
2020-03-13 22:23:01 +0800

    初始化项目

--- spring-security-core/src/main/java/com/wiliam/security/controller/DemoController.java
+++ spring-security-core/src/main/java/com/wiliam/security/controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.erqiang.security.controller;
+package com.wiliam.security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
